4aa192d5732fc2d1061e7c31901d29bc929a6864
Logan Kottler <lkottler@wisc.edu>
2020-12-17 17:00:42 -0600

    Updated collision to work properly
    wrote script to navigate to all leaf nodes of gameobject, and add meshcollider

---
+++ Assets/Scripts/ColliderCreator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderCreator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject collisionObjects;
+
+    void Awake()
+    {
+        MeshRenderer[] allChildren = collisionObjects.GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer m in allChildren)
+        {
+            if (m.gameObject.GetComponent<MeshCollider>() == null)
+                m.gameObject.AddComponent<MeshCollider>();
+        }
+    }
+}
