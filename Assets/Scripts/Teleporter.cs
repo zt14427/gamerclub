87d257f4c148ecad34642539947a7687a16a68be
Logan Kottler <lkottler@wisc.edu>
2021-01-05 17:36:40 -0600

    Created a working teleporter prefab (no graphic)

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Teleporter : MonoBehaviour
+{
+    Transform teleportLink;
+    private void Start()
+    {
+        teleportLink = transform.GetChild(0).transform;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collided, transport to: " + teleportLink.position);
+        // Only if the object is a server-side object teleport
+        if (other.gameObject.GetComponent<PhotonView>()) {
+            CharacterController cc = other.GetComponent<CharacterController>();
+
+            Debug.Log("teleporting: "  + other.transform.position);
+            cc.enabled = false;
+            other.transform.position = teleportLink.position;
+            cc.enabled = true;
+        }
+    }
+
+}
