2bc793e5a0cdf0125b3356dfbf80c7ee2d80a6b0
Logan Kottler <lkottler@wisc.edu>
2021-01-09 00:31:10 -0600

    Added skeleton merchant in castle

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rb;
+
+    [SerializeField]
+    private float openForce = 10.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger entered: " + other.transform.name);
+        if (other.transform.name.Contains("PLAYER"))
+        {
+            if (other.gameObject.GetComponent<PhotonView>().IsMine)
+            {
+                Vector3 dir = (other.transform.position - transform.position).normalized;
+                rb.AddForce(-dir * openForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
