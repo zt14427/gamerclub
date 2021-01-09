72ed0bd587ac9b3458920d14798ca5a5040d87e1
Logan Kottler <lkottler@wisc.edu>
2021-01-09 17:12:13 -0600

    Attempted to add a soccer ball

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public static float kickForce = 20f;
+
+    private AudioSource kick;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(Physics.gravity * 2f, ForceMode.Acceleration);
+        kick = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collider other)
+    {
+        if (other.transform.name.Contains("PLAYER"))//.tag == "Player")
+        {
+            Vector3 direction = (other.transform.position - transform.position).normalized;
+            rb.AddForce(-direction * kickForce, ForceMode.Impulse);
+            kick.Play();
+        }
+    }
+}
