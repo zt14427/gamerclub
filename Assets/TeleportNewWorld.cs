317e04cdcd9ec00b21994256a95e34517c5e5c87
Logan Kottler <lkottler@wisc.edu>
2021-01-19 14:25:36 -0600

    Added new teleport out of scene

---
+++ Assets/TeleportNewWorld.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TeleportNewWorld : MonoBehaviour
+{
+    public int unlock = 0;
+    Transform teleportLink;
+    private void Start()
+    {
+        teleportLink = transform.GetChild(0).transform;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // Only if the object is a server-side object teleport
+        if (other.gameObject.GetComponent<PhotonView>() && LocalStats.unlockables[unlock])
+        {
+            CharacterController cc = other.GetComponent<CharacterController>();
+            cc.enabled = false;
+            other.transform.position = teleportLink.position;
+            cc.enabled = true;
+        }
+    }
+}
