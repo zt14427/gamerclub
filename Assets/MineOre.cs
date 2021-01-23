4759a17ee78aa5de25fd68d2c30206c29228291d
Logan Kottler <lkottler@wisc.edu>
2021-01-23 16:13:44 -0600

    Began implementation of Mining

---
+++ Assets/MineOre.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineOre : MonoBehaviour
+{
+    [SerializeField]
+
+
+    private GameObject canvas;
+    private void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Photon.Pun.PhotonView>().IsMine)
+        {
+
+        }
+
+    }
+
+}
