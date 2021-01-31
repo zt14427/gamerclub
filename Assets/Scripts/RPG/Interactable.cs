371e2dd14c0e4ada5ae5b4ea2a975b92c5b9ce0b
Logan Kottler <lkottler@wisc.edu>
2021-01-30 19:07:47 -0600

    Made a basic interactable class

---
+++ Assets/Scripts/RPG/Interactable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PhotonView pv = other.GetComponent<PhotonView>();
+        if (pv != null && pv.IsMine)
+        {
+            Debug.Log("player can pickup item");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        PhotonView pv = other.GetComponent<PhotonView>();
+        if (pv != null && pv.IsMine)
+        {
+            Debug.Log("player can no longer pickup item");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        PhotonView pv = other.GetComponent<PhotonView>();
+        if (pv != null && pv.IsMine && Input.GetKey("f"))
+        {
+            Debug.Log("player pressed f");
+        }
+    }
+}
